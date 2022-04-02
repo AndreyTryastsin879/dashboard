ecfbc2269bbb3bfd7d8311ed737d8cd87828954d
Andrey <tryastsinandrei@yandex.ru>
2022-04-02 12:34:30 +0300

    improve config add settings

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
-class Configuration(object):
+from settings import DB_NAME, DB_USER, DB_HOST, DB_PASS, DB_PORT
+
+
+class Configuration:
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:root@localhost/mrnr_dashboard_db'
+    SQLALCHEMY_DATABASE_URI = f'mysql+mysqlconnector://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
     SECRET_KEY = 'secret'
 
     SECURITY_PASSWORD_SALT = 'salt'
